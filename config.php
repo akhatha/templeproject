254006c70aa7d96405664643fcbecd5f51c20b99
akshatha <akshatha.k@glowtouch.com>
2017-09-21 12:56:50 +0530

    config file

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
-define('DB_USER', "nittemobileapp"); // db user  
-define('DB_PASSWORD', "9kmd9sjdu"); // db password  
-define('DB_DATABASE', "nittemobileapp"); // database name  
-define('DB_SERVER', "192.168.12.210"); // db server/ host name  
+define('DB_USER', "root"); // db user  
+define('DB_PASSWORD', ""); // db password  
+define('DB_DATABASE', "templeproject"); // database name  
+define('DB_SERVER', "localhost"); // db server/ host name  
 $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());  
 $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error()); 
 define("STATUS", "success");
