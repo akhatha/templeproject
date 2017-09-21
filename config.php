f35a921b9c8069150cccfb4859aca95ed4792e3f
HP-PC\HP <gauthami9111@gmail.com>
2017-09-22 01:34:35 +0530

    no message

--- config.php
+++ config.php
@@ -7,12 +7,9 @@ define('DB_SERVER', "localhost"); // db server/ host name
 $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());  
 $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error()); 
 define("STATUS", "success");
-<<<<<<< HEAD
+
 
 define("SITE_URL",$_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST']."/temple");
-=======
-define("SITE_URL",$_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST']."/nittemobileapp");
->>>>>>> parent of 30570a3... video and gallery
 
 
 ?>
